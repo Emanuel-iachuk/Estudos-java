3f36114ee03fdd7dc9aff62325f5ef9c983cfe90
Emanuel iachuk <emanueliachuk@gmail.com>
2025-12-22 19:22:48 -0300

    meu primeiro progeto

--- Meu-primeiro-programa/src/App.java
+++ Meu-primeiro-programa/src/App.java
@@ -1,5 +1,12 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+    public static int soma(int n1, int n2){
+        int somar = n1 + n2;
+        System.out.println("Resutado: " + somar);
+        return somar;
+    }
+
+    public static void main(String[] args){
+        soma(10, 17);
     }
 }
