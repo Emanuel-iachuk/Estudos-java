d85babd6c91e894177776d3746e9b90c3e0d775b
Emanuel iachuk <emanueliachuk@gmail.com>
2025-12-23 17:33:42 -0300

    meu primeiro progeto v1.2

--- Meu-primeiro-programa/src/App.java
+++ Meu-primeiro-programa/src/App.java
@@ -1,12 +1,33 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class App {
+    static Scanner snc = new Scanner(System.in);
 
     public static int soma(int n1, int n2){
-        int somar = n1 + n2;
-        System.out.println("Resutado: " + somar);
-        return somar;
+        return n1 + n2;
     }
 
     public static void main(String[] args){
-        soma(10, 17);
+        while(true){
+            try {
+                System.out.println("Digite um numero:");
+                int numero1 = snc.nextInt();
+                System.out.println("Digite outro numero:");
+                int numero2 = snc.nextInt();
+                int resultado = soma(numero1, numero2);
+
+                System.out.println("Resultado: " + resultado);
+
+                snc.nextLine();
+
+                break;
+            } catch (InputMismatchException e) {
+                System.out.println("❌ Digite APENAS números válidos!");
+                snc.nextLine();
+
+            }
+        }
+        snc.close();
     }
 }
