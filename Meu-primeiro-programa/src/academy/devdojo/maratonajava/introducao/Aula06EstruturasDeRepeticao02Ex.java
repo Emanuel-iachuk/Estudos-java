12d10e9930ff2fa5309b975b27ee87fd6ff4702a
Emanuel-iachuk <emanueliachuk6@gmail.com>
2026-01-06 19:09:15 -0300

    Refaturação do codigo for que pula de 100 em 100

--- Meu-primeiro-programa/src/academy/devdojo/maratonajava/introducao/Aula06EstruturasDeRepeticao02Ex.java
+++ Meu-primeiro-programa/src/academy/devdojo/maratonajava/introducao/Aula06EstruturasDeRepeticao02Ex.java
@@ -3,6 +3,7 @@ package academy.devdojo.maratonajava.introducao;
 
 public class Aula06EstruturasDeRepeticao02Ex {
     public static void main(String[] args) {
+        //for que pula de 100 em 100
         for (int i = 0; i <= 1000000; i+= 100) {
             if (i % 2 == 0) {
                 System.out.println("Par: " + i);
