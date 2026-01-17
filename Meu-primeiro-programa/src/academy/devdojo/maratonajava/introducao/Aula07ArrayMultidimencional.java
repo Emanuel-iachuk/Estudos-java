9e66764117bf8fb2051bff215f03d35d575867dd
Emanuel Iachuk <emanueliachuk6@gmail.com>
2026-01-16 21:25:30 -0300

    Array multidimencionais for normal

--- Meu-primeiro-programa/src/academy/devdojo/maratonajava/introducao/Aula07ArrayMultidimencional.java
+++ Meu-primeiro-programa/src/academy/devdojo/maratonajava/introducao/Aula07ArrayMultidimencional.java
@@ -1,7 +1,5 @@
 package academy.devdojo.maratonajava.introducao;
 
-import java.util.Arrays;
-
 public class Aula07ArrayMultidimencional {
     public static void main(String[] args) {
         //Array multidimencional
@@ -11,12 +9,22 @@ public class Aula07ArrayMultidimencional {
 
         //For para percorer o Array multidimencional
         for (int i = 0; i < notas.length; i++) {
-            //Soma as notas de todos os alunos
-            media[i] = Arrays.stream(notas[i]).sum() / 3;
-            System.out.println("Notas: "+media[i]);
+            double soma = 0;
+            for (int j = 0; j < notas[i].length; j++) {
+                //Soma as notas de todos os alunos
+                soma += notas[i][j];
+            }
+            media[i] = soma / 3;
+            System.out.println(media[i]);
         }
-        //Soma todas as notas da sala
-        double mediaSala = Arrays.stream(media).sum() / 3;
-        System.out.println(mediaSala);
+
+        //Soma a media da sala
+        double somaMediaSala = 0;
+        for (int i = 0; i < media.length; i++) {
+            somaMediaSala += media[i];
+        }
+        double mediaSala = somaMediaSala / 3;
+        System.out.println("Media da sala: "+mediaSala);
+
     }
 }
